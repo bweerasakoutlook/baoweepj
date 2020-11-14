5e8c02926e11533e0c354f74c301713433b80a09
weerasak bunsuk <bweerasak@gmail.com>
2020-11-14 21:49:42 +0700

    first again

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -1,14 +1,59 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, View, ActivityIndicator, FlatList} from 'react-native';
 
-const DetailScreen = () => {
-    return (
-        <View>
-            <Text>Product Detail</Text>
-        </View>
-    )
-}
+import axios from 'axios';
+import {
+  Container,
+  Header,
+  Content,
+  List,
+  ListItem,
+  Thumbnail,
+  Text,
+  Left,
+  Body,
+  Right,
+  Button,
+  Badge,
+} from 'native-base';
 
-export default DetailScreen
+const DetailScreen = ({navigation, route}) => {
+  const {id, title} = route.params;
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      title: 'Product very detail',
+    });
+  }, [navigation, title]);
 
-const styles = StyleSheet.create({})
+  const [detail, setDetail] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const getData = async (id) => {
+    try {
+      setLoading(true);
+      const res = await axios.get(
+        'https://api.codingthailand.com/api/course/' + id,
+      );
+      // console.log(res.data.data);
+      // alert(JSON.stringify(res.data.data));
+      setDetail(res.data.data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setError(error);
+    }
+  };
+
+  return (
+    <View>
+      <Text>
+        {id} {title}
+      </Text>
+    </View>
+  );
+};
+
+export default DetailScreen;
+
+const styles = StyleSheet.create({});
